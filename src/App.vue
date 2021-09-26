03f51466cc5c658767f7e6be1f0b2ea551669507
Brandon Franks <bgfranks.dev@gmail.com>
2021-09-26 13:29:46 -0400

    created a mobile screen check for the application

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <div class="app flex">
+    <div v-if="!mobile" class="app flex">
       <Navigation />
       <div class="app-content flex flex-column">
         <router-view />
       </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or tablet</p>
     </div>
   </div>
 </template>
@@ -13,8 +17,29 @@
 import Navigation from './components/Navigations.vue';
 
 export default {
+  data() {
+    return {
+      mobile: null,
+    };
+  },
   components: {
     Navigation,
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener('resize', this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWitdh = window.innerWidth;
+
+      if (windowWitdh <= 750) {
+        this.mobile = true;
+        return;
+      }
+
+      this.mobile = false;
+    },
   },
 };
 </script>
@@ -42,6 +67,19 @@ export default {
     flex: 1;
     padding: 0 20px;
     position: relative;
+  }
+}
+
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #fff;
+
+  p {
+    margin-top: 16px;
   }
 }
 
