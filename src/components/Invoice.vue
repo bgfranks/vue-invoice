e1e16bd4474daa3e2e34063573e866bf6b91714e
Brandon Franks <bgfranks.dev@gmail.com>
2021-09-28 23:23:37 -0400

    created the invoice component to display each item from the firestore

---
+++ src/components/Invoice.vue
@@ -0,0 +1,36 @@
+<template>
+  <router-link :to="{}" class="flex invoice">
+    <div class="left flex">
+      <span class="tracking-number">#{{ invoice.invoiceId }}</span>
+      <span class="due-date">{{ invoice.paymentDueDate }}</span>
+      <span class="person">{{ invoice.clientName }}</span>
+    </div>
+    <div class="right flex">
+      <span class="price">${{ invoice.invoiceTotal }}</span>
+      <div
+        class="status-button flex"
+        :class="{
+          paid: invoice.invoicePaid,
+          draft: invoice.invoiceDraft,
+          pending: invoice.invoicePending,
+        }"
+      >
+        <span v-if="invoice.invoicePaid">Paid</span>
+        <span v-if="invoice.invoicePending">Pending</span>
+        <span v-if="invoice.invoiceDraft">Draft</span>
+      </div>
+      <div class="icon">
+        <img src="@/assets/icon-arrow-right.svg" alt="right arrow" />
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'invoice',
+  props: ['invoice'],
+};
+</script>
+
+<style></style>
