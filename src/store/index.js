605b680762c097cef5343573ae5b91c3979310c6
Brandon Franks <bgfranks.dev@gmail.com>
2021-09-29 21:08:16 -0400

    wrote the functionality to dynaimcially display invoice info in the invoice page

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,7 @@ export default createStore({
     modalActive: null,
     invoiceData: [],
     invoicesLoaded: null,
+    currentInvoiceArray: null,
   },
   mutations: {
     TOGGLE_INVOICE(state) {
@@ -21,6 +22,11 @@ export default createStore({
     },
     INVOICES_LOADED(state) {
       state.invoicesLoaded = true;
+    },
+    SET_CURRENT_INVOICE(state, payload) {
+      state.currentInvoiceArray = state.invoiceData.filter((invoice) => {
+        return invoice.invoiceId === payload;
+      });
     },
   },
   actions: {
--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -1,10 +1,31 @@
 <template>
-  <div></div>
+  <div>{{ currentInvoice.invoiceId }}</div>
 </template>
 
 <script>
+import { mapMutations, mapState } from 'vuex';
+
 export default {
   name: 'invoiceView',
+  data() {
+    return {
+      currentInvoice: null,
+    };
+  },
+  created() {
+    this.getCurrentInvoice();
+  },
+  methods: {
+    ...mapMutations(['SET_CURRENT_INVOICE']),
+
+    getCurrentInvoice() {
+      this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
+      this.currentInvoice = this.currentInvoiceArray[0];
+    },
+  },
+  computed: {
+    ...mapState(['currentInvoiceArray']),
+  },
 };
 </script>
 
